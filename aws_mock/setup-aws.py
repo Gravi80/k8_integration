d9f483ebc76b839470c980c1c5d0d36208be214b
Ravi Sharma <ravis4376@gmail.com>
2020-06-06 21:51:55 +0200

    refactoring

--- aws_mock/setup-aws.py
+++ aws_mock/setup-aws.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import os
 import time
-SEED_DATA_DIR="/s3/seed-data"
+
+
+# Seed AWS S3 buckets
+SEED_DATA_DIR="/aws/s3/seed-data"
 ASW_URL="http://localstack:4566"
 
 time.sleep(10)
 for bucket in os.listdir(SEED_DATA_DIR):
 	if os.path.isdir(os.path.join(SEED_DATA_DIR, bucket)):
+		print("Creating bucket {0}".format(bucket))
 		os.system("aws --endpoint-url={s3_url} s3 mb s3://{bucket_name}".format(
 			s3_url=ASW_URL,bucket_name=bucket))
 		
 		os.system("aws --endpoint-url={s3_url} s3 cp {seed_dir}/{bucket_name} s3://{bucket_name} --recursive".format(
 			s3_url=ASW_URL,bucket_name=bucket,seed_dir=SEED_DATA_DIR))
 
-print("Seed completed")
+print("AWS S3 buckets seed completed")
