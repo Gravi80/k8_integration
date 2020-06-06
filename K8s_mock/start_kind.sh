cd19f6e83f02e4a2abda171cf21995026f0f8fb3
Ravi Sharma <ravis4376@gmail.com>
2020-06-06 18:00:54 +0200

    Basic docker compose

--- K8s_mock/start_kind.sh
+++ K8s_mock/start_kind.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+set -x
 CLUSTER_NAME=${1:-local-cluster}
 #By default, the cluster access configuration is stored in ${HOME}/.kube/config if $KUBECONFIG environment variable is not set.
 #We will keep kind cluster config in a separate config file
+
+# SIGTERM-handler
+term_handler() {
+  kind delete cluster --name $CLUSTER_NAME
+}
+
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+
+kind delete cluster --name $CLUSTER_NAME
 if [ ! -d "/configs/kube-config" ]; then
   mkdir /configs/kube-config
 fi
@@ -13,3 +26,9 @@ export KUBECONFIG="/configs/kube-config/config"
 # This will bootstrap a Kubernetes cluster using a pre-built node image
 # you can find it on docker hub kindest/node.
 kind create cluster --config /configs/cluster-config.yaml --name $CLUSTER_NAME
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
